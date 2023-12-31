b544316596ac9923de068b87008f8d1829421f6c
Sojan James <sojan.james@gmail.com>
2023-12-31 10:57:22 +0530

    Target tests

--- threadx-app/cross/target-tests/tests/stm32f103.rs
+++ threadx-app/cross/target-tests/tests/stm32f103.rs
@@ -1,5 +1,34 @@
 #![no_main]
 #![no_std]
 
+// ThreadX tests running on an stm32f103 bluepill board
+
 use defmt_rtt as _;
 use panic_probe as _;
+use stm32f1xx_hal as _; // memory layout
+
+use threadx_rs::Builder;
+
+#[defmt_test::tests]
+mod tests {
+    use core::time::Duration;
+    use defmt::{assert_eq, unwrap};
+    use threadx_rs::Builder;
+
+    struct Board{}
+
+
+    #[init]
+    fn init() -> Board {
+
+        //let tx = Builder::new(low_level_init_cb, app_define_cb)
+
+        Board {  }
+    }
+
+    #[test]
+    fn confirm_firmware_version(board: &mut Board) {
+        
+    }
+
+}
