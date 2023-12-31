4c36f7ea0f99e7ce9179d46e9e82a4bd141c9f42
Sojan James <sojan.james@gmail.com>
2023-12-31 11:23:04 +0530

    Comments

--- threadx-app/host-target-tests/src/main.rs
+++ threadx-app/host-target-tests/src/main.rs
@@ -1,3 +1,7 @@
+// combined host and target tests. The target image of the app is flashed to the target
+// and this host application is run on the host. The host application communicates with the
+// target application via some mechanism such as serial port or network. This enables end to end testing
+
 fn main() {
     println!("Hello, world!");
 }
